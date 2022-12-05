739ec2898a0c5b6d84ecc4fac3e1dc85f91d5c04
jtsw1990 <jtsw1990@gmail.com>
2022-12-05 14:10:35 +1100

    updated comments

--- test_run.py
+++ test_run.py
@@ -11,6 +11,17 @@ client = tweepy.Client(bearer_token, wait_on_rate_limit=True)
 
 query = 'covid OR covid19 OR covid-19 -is:retweet'
 
-response = client.search_recent_tweets(query=query, max_results=100)
+#response = client.search_recent_tweets(query=query, max_results=100, tweet_fields=['created_at', 'lang'], user_fields=['profile_image_url'], expansions=['author_id'])
 
-print(response)
+
+for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, max_results=100).flatten(limit=1000):
+    print(tweet.id)
+
+
+#users = { k['id']: k for k in response.includes['users'] }
+
+# for tweet in response.data:
+#     if users[tweet.author_id]:
+#         user = users[tweet.author_id]
+#         print(tweet.id)
+#         print(user.profile_image_url)
